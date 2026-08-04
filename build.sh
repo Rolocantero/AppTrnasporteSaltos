c2a243ca3cf8859fc606bbab004289763f5b73cb
Rolocantero <rolocan2685@gmail.com>
2026-08-04 18:05:10 -0300

    Auto create superuser admin on build

--- build.sh
+++ build.sh
@@ -4,3 +4,12 @@ set -o errexit
 pip install -r requirements.txt
 python backend/manage.py migrate
 python backend/manage.py collectstatic --no-input
+
+# Crear superusuario admin si no existe
+python backend/manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@viajaya.com', 'admin1234')
+    print('Superusuario admin creado con éxito.')
+"
